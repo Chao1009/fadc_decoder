67dad9eaa9cbb651a5661bd0ff7b5b28718b93eb
Chao Peng <cpeng@anl.gov>
2020-10-04 20:28:07 -0500

    initial commit

---
+++ rootlogon.C
@@ -0,0 +1,13 @@
+{
+    gSystem->SetBuildDir(".root_build_dir", true);
+    // analyzer
+    gInterpreter->AddIncludePath("./decoder");
+    gSystem->Load("./build/decoder/libfdec.so");
+
+    // online monitor
+    /*
+    gInterpreter->AddIncludePath("./online");
+    gInterpreter->AddIncludePath("./third_party/et/include/coda/et");
+    gSystem->Load("./third_party/et/lib64/libcoda_et.so");
+    */
+}
